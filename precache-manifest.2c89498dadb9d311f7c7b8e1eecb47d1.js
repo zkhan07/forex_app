09a983374afaebd051598eac2df7b4957dc5a540
zaid <khanzaid786786.kz@gmail.com>
2020-01-14 15:51:11 +0530

    Updates

---
+++ precache-manifest.2c89498dadb9d311f7c7b8e1eecb47d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a233112cbfd56ee4a5bf6e5b929498",
+    "url": "/forex_app/index.html"
+  },
+  {
+    "revision": "1f2601441f9bf277db51",
+    "url": "/forex_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "54f40d2402cb99f1ffbe",
+    "url": "/forex_app/static/js/2.a4c790a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/forex_app/static/js/2.a4c790a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f2601441f9bf277db51",
+    "url": "/forex_app/static/js/main.e90f2f44.chunk.js"
+  },
+  {
+    "revision": "1f092de5376089777955",
+    "url": "/forex_app/static/js/runtime-main.4b6c3408.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/forex_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
